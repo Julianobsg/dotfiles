79e32a8febb601855e9fa27c7992e2c70eaf3d58
Juliano Gomes <julianobsg@gmail.com>
2026-04-10 18:25:06 -0300

    Adds rg shortcut

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -3,3 +3,11 @@ local map = vim.keymap.set
 map("n", "<C-\\>", "<cmd>Neotree toggle<cr>", { silent = true, desc = "Explorer" })
 map("n", ",t", "<cmd>Telescope find_files<cr>", { silent = true, desc = "Find files" })
 map("n", ",g", "<cmd>Telescope live_grep<cr>", { silent = true, desc = "Live grep" })
+
+vim.api.nvim_create_user_command("Rg", "silent grep! <args> | copen", {
+  nargs = "+",
+  complete = "file_in_path",
+  desc = "Search with ripgrep into quickfix",
+})
+
+vim.cmd([[cnoreabbrev <expr> rg getcmdtype() == ':' && getcmdline() ==# 'rg' ? 'Rg' : 'rg']])
